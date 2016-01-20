9c9a63cc7429b8d16992f1fe1cf7d9e4eb4df890
Haider <C02878A@uk.experian.local>
2016-01-20 18:57:35 +0000

    node

--- index.js
+++ index.js
@@ -1,37 +1,10 @@
-var cool = require('cool-ascii-faces');
-var express = require('express');
-var app = express();
+var express = require('express')
+var app = express()
+ 
+// app.get('/', function (req, res) {
+//   res.send('Hello World')
+// })
 
-app.set('port', (process.env.PORT || 5000));
-
-app.use(express.static(__dirname + '/public'));
-
-// views is directory for all template files
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
-
-app.get('/', function(request, response) {
-  response.render('pages/index');
-});
-
-app.get('/cool', function(request, response) {
-  response.send(cool());
-});
-
-app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
-});
-
-var pg = require('pg');
-
-app.get('/db', function (request, response) {
-  pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-    client.query('SELECT * FROM test_table', function(err, result) {
-      done();
-      if (err)
-       { console.error(err); response.send("Error " + err); }
-      else
-       { response.render('pages/db', {results: result.rows} ); }
-    });
-  });
-})
+app.get('/', (req, res) => res.send('Hello World') );
+ 
+app.listen(3000)
