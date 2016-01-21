55cd1e44ce4d30223d7c5452076771ce73291c51
Haider <C02878A@uk.experian.local>
2016-01-21 11:21:19 +0000

    gulp

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,7 @@ gulp.task('express', function() {
   var express = require('express');
   var app = express();
   app.use(express.static(__dirname));
-  app.listen(4000);
+  // app.listen(4000);
 });
 
 gulp.task('default', ['express'], function() {
