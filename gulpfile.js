9d2822289b3004ae33604757edac70cb53d41900
Haider <C02878A@uk.experian.local>
2016-01-21 11:10:59 +0000

    adding files

---
+++ gulpfile.js
@@ -0,0 +1,12 @@
+var gulp = require('gulp');
+
+gulp.task('express', function() {
+  var express = require('express');
+  var app = express();
+  app.use(express.static(__dirname));
+  app.listen(4000);
+});
+
+gulp.task('default', ['express'], function() {
+
+});
