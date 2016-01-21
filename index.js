0edb73c9e90df2580028980e95482fb6866431c7
Haider <C02878A@uk.experian.local>
2016-01-21 16:47:36 +0000

    changed port

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 var express = require('express');
 var app = express();
 
+app.set('port', (process.env.PORT || 5000));
+
 app.get('/', function (req, res) {
   res.send('Hello World!');
 });
 
-app.listen(4000, function () {
-  console.log('Example app listening on port 4000!');
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
 });
