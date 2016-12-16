329b02e3ccad8f17a0980e1703bde08ab55f6791
Ammar Haider <ammar.haider@oakbrook.local>
2016-12-16 21:18:06 +0000

    fixing main layout html

--- server/views/layouts/default.jsx
+++ server/views/layouts/default.jsx
@@ -12,6 +12,12 @@ const DefaultLayout = React.createClass({
     return (
       <html>
         <head>
+          <meta charset="utf-8" />
+          <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+          <meta name="description" content="" />
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <link rel="apple-touch-icon" href="apple-touch-icon.png" />
+          <link href='https://fonts.googleapis.com/css?family=Roboto' rel='stylesheet' type='text/css' />
           <title>{this.props.title}</title>
           <link rel="stylesheet" type="text/css" href={ `${this.props.commonCss}` } />
           <link rel="stylesheet" type="text/css" href={ `${this.props.cssFile}` } />
