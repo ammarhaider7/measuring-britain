5998ffcfeadb79f9b48f07cf3bb96579adbe1d11
ammar.haider <h.ammar.haider@gmail.com>
2016-12-22 16:52:33 +0000

    navbar burger transition

--- server/views/partials/navbar.jsx
+++ server/views/partials/navbar.jsx
@@ -4,22 +4,11 @@ const Nav = React.createClass({
 
   render() {
     return (
-		<nav className="navbar mb-navbar navbar-light bg-faded">
-			<button className="navbar-toggler hidden-sm-up" type="button" data-toggle="collapse" data-target="#nav-content">
-			</button>
-			<div className="collapse navbar-toggleable-xs" id="nav-content">
-				<a className="navbar-brand" href="#">Logo</a>
-				<ul className="nav navbar-nav">
-					<li className="nav-item">
-						<a className="nav-link" href="#">Link 1</a>
-					</li>
-					<li className="nav-item">
-						<a className="nav-link" href="#">Link 2</a>
-					</li>
-					<li className="nav-item">
-						<a className="nav-link" href="#">Link 3</a>
-					</li>
-				</ul>
+		<nav className="mb-navbar">
+			<div className="navbar-icon hidden-sm-up">
+				<span></span>
+				<span></span>
+				<span></span>
 			</div>
 		</nav>
     );
