630a902b9c37ddbec8332cad1329e0a4db728810
ammar.haider <h.ammar.haider@gmail.com>
2016-12-22 15:43:57 +0000

    box-shadow under nav and fixing nodemon for server changes

--- server/views/partials/navbar.jsx
+++ server/views/partials/navbar.jsx
@@ -4,7 +4,7 @@ const Nav = React.createClass({
 
   render() {
     return (
-		<nav className="navbar navbar-light bg-faded">
+		<nav className="navbar mb-navbar navbar-light bg-faded">
 			<button className="navbar-toggler hidden-sm-up" type="button" data-toggle="collapse" data-target="#nav-content">
 			</button>
 			<div className="collapse navbar-toggleable-xs" id="nav-content">
@@ -18,14 +18,6 @@ const Nav = React.createClass({
 					</li>
 					<li className="nav-item">
 						<a className="nav-link" href="#">Link 3</a>
-					</li>
-					<li className="nav-item dropdown">
-						<a className="nav-link dropdown-toggle" href="http://example.com" id="supportedContentDropdown" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Dropdown</a>
-						<div className="dropdown-menu" aria-labelledby="supportedContentDropdown">
-							<a className="dropdown-item" href="#">Action</a>
-							<a className="dropdown-item" href="#">Another action</a>
-							<a className="dropdown-item" href="#">Something else here</a>
-						</div>
 					</li>
 				</ul>
 			</div>
