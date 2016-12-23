d83b7f0d74d3d91bd3bd6d194d4fa61d70e49e25
ammar.haider <h.ammar.haider@gmail.com>
2016-12-23 15:58:53 +0000

    applying vendorjs to about page

--- server/views/about.jsx
+++ server/views/about.jsx
@@ -3,11 +3,18 @@ const DefaultLayout = require('./layouts/default');
 
 const About = React.createClass({
 
+	propTypes: {
+		commonJs: React.PropTypes.string,
+		vendorJs: React.PropTypes.string,
+		jsFile: React.PropTypes.string
+	},
+
   render() {
     return (
 		<DefaultLayout {...this.props}>
 			<div id="body"></div>
 			<script src={ `${this.props.commonJs}` }></script>
+			<script src={ `${this.props.vendorJs}` }></script>
 			<script src={ `${this.props.jsFile}` }></script>
 		</DefaultLayout>
     );
