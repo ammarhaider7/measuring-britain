4bb087a4eb299f0355db95f9b16f22d1877150ee
Ammar Haider <h.ammar.haider@gmail.com>
2017-07-13 09:48:51 +0100

    adds newrelic.js to root

---
+++ newrelic.js
@@ -0,0 +1,26 @@
+'use strict'
+
+/**
+ * New Relic agent configuration.
+ *
+ * See lib/config.default.js in the agent distribution for a more complete
+ * description of configuration variables and their potential values.
+ */
+exports.config = {
+  /**
+   * Array of application names.
+   */
+  app_name: ['Measuring Britain'],
+  /**
+   * Your New Relic license key.
+   */
+  license_key: '61edc8b845d52bb6d6c4e1153091bcc95a7780cb',
+  logging: {
+    /**
+     * Level at which to log. 'trace' is most useful to New Relic when diagnosing
+     * issues with the agent, 'info' and higher will impose the least overhead on
+     * production applications.
+     */
+    level: 'info'
+  }
+}
